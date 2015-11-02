9662434f0bebcbe91bac0929a589b1a603c7f275
lubuntu <lubuntu@lubuntu>
2015-11-02 22:46:04 +0000

    redo the changes on the server

--- server.py
+++ server.py
@@ -13,14 +13,7 @@ def home():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
 
-@app.route('/coaches')
-def coaches():
-    now = datetime.datetime.now()
-    return render_template('coaches.html', current_time=now.ctime())
 
-@app.route('/players')
-def movies_page():
-    return render_template('players.html')
 
 
 if __name__ == '__main__':
