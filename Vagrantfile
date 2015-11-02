f7782729412881f6c702c34b2d7abe8441eda4df
mertana <alican.mertan@hotmail.com>
2015-11-02 03:31:16 +0200

    1

--- Vagrantfile
+++ Vagrantfile
@@ -41,4 +41,5 @@ Vagrant.configure("2") do |config|
     config.vm.provision :shell do |shell|
         shell.path = "vagrant-manifests/phppgadmin.sh"
     end
+    config.vm.boot_timeout = 1000 
 end
--- server.py
+++ server.py
@@ -13,6 +13,15 @@ def home():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
 
+@app.route('/coaches')
+def coaches():
+    now = datetime.datetime.now()
+    return render_template('coaches.html', current_time=now.ctime())
+
+@app.route('/players')
+def movies_page():
+    return render_template('players.html')
+
 
 if __name__ == '__main__':
     PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
