4295c335d071195a86e02098671fa85dd48e46d0
ylmazahmt <ylmazahmt@gmail.com>
2015-11-03 04:48:19 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+import datetime
+import os
+
+from flask import Flask
+from flask import render_template
+
+from config import app
+import coaches
+import referees
+import tournaments
+
+@app.route('/')
+def home():
+    now = datetime.datetime.now()
+    return render_template('home.html', current_time=now.ctime())
+
+
+
+
+if __name__ == '__main__':
+
+    PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
+    app.run(host='0.0.0.0', port=int(PORT))
