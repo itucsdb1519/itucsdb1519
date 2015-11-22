928482a542da2c1a8f55024aa7f1294d8b03b0e1
Alican Mertan <alican.mertan@hotmail.com>
2015-11-22 18:38:59 +0200

    added update feature in mathces.py and matches.html

--- matches.py
+++ matches.py
@@ -13,6 +13,7 @@ def create_table():
     try:
         cursor = create_connection()
         statement = """ CREATE TABLE MATCHES(
+        ID SERIAL PRIMARY KEY,
         TOURNAMENT VARCHAR(45),
         TEAM1 VARCHAR(10),
         TEAM2 VARCHAR(20),
@@ -48,9 +49,9 @@ def add_new_match(tournament, team1, team2, winner, date):
 
     return True
 
-def delete_match(tournament):
+def delete_match(id):
     cursor = create_connection()
-    statement = """DELETE FROM MATCHES WHERE TOURNAMENT='{}'""".format(tournament)
+    statement = """DELETE FROM MATCHES WHERE ID={}""".format(id)
     cursor.execute(statement)
     cursor.connection.commit()
 
@@ -67,6 +68,14 @@ def find_match(tournamentFind, team1Find, team2Find, winnerFind, dateFind):
     close_connection(cursor)
 
     return matches
+
+def update_match(id, tournamentUpdate, team1Update, team2Update, winnerUpdate, dateUpdate):
+    cursor = create_connection()
+    statement = """UPDATE MATCHES SET TOURNAMENT = '{}', TEAM1 = '{}', TEAM2 = '{}', WINNER = '{}', DATE = '{}' WHERE ID={} """.format(tournamentUpdate, team1Update, team2Update, winnerUpdate, dateUpdate, id)
+    cursor.execute(statement)
+    cursor.connection.commit()
+
+    close_connection(cursor)
 
 @app.route("/matches", methods=['GET', 'POST'])
 def matches():
@@ -91,9 +100,9 @@ def matches():
         all_matches = get_matches() # get all matches
         queriedMatches = find_match('?','?','?','?','?')
     elif 'delete' in request.form:
-        tournaments = request.form.getlist('matches_to_delete')
-        for tournament in tournaments:
-            delete_match(tournament)
+        ids = request.form.getlist('matches_to_delete')
+        for id in ids:
+            delete_match(id)
         all_matches = get_matches()
         queriedMatches = find_match('?','?','?','?','?')
     elif 'find' in request.form:
@@ -105,4 +114,16 @@ def matches():
 
         all_matches = get_matches()
         queriedMatches = find_match(tournamentFind, team1Find, team2Find, winnerFind, dateFind)
+    elif 'update' in request.form:
+        ids = request.form.getlist('update')
+        for id in ids:
+            tournamentUpdate = request.form['tournamentUpdate'+id]
+            team1Update = request.form['team1Update'+id]
+            team2Update = request.form['team2Update'+id]
+            winnerUpdate = request.form['winnerUpdate'+id]
+            dateUpdate = request.form['dateUpdate'+id]
+            update_match(id, tournamentUpdate, team1Update, team2Update, winnerUpdate, dateUpdate)
+
+        all_matches = get_matches()
+        queriedMatches = find_match('?','?','?','?','?')
     return render_template("matches.html", matches=all_matches, matchesToShow=queriedMatches)
