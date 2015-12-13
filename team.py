9895c898c6188e50b2e88b5fdeb2fa5bf0a16b6f
namlih <hasanburaknamli@gmail.com>
2015-12-13 22:10:09 +0200

    added team.py file

---
+++ team.py
@@ -0,0 +1,6 @@
+class team:
+    def __init__(self, nation, gender, foundDate, timesWon):
+        self.nation = nation
+        self.gender = gender
+        self.foundDate = foundDate
+        self.timesWon = timesWon
