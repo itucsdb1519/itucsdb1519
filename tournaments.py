ae87813d7cb381cd527a4b822087d3c9d71ddbe6
Alican Mertan <alican.mertan@hotmail.com>
2015-12-13 05:22:13 +0200

    added foreign key to tournaments table

--- tournaments.py
+++ tournaments.py
@@ -10,6 +10,7 @@ from store import StoreP
 
 import psycopg2
 import players
+import teams
 
 
 
@@ -20,8 +21,7 @@ def create_table():
         ID SERIAL PRIMARY KEY,
         NAME VARCHAR(45),
         YEAR VARCHAR(4),
-        WINNER VARCHAR(45),
-        SECOND_PLACE  VARCHAR(20),
+        WINNER INTEGER REFERENCES TEAMS ON DELETE CASCADE ON UPDATE CASCADE,
         BEST_PLAYER INTEGER REFERENCES PLAYERS ON DELETE CASCADE ON UPDATE CASCADE
         )"""
         cursor.execute(statement)
@@ -43,10 +43,10 @@ def get_tournaments():
     return tournaments
 
 
-def add_new_tournament(name, year, winner, second_place, best_player):
+def add_new_tournament(name, year, winner, best_player):
     cursor = create_connection()
 
-    cursor.execute("INSERT INTO tournaments (name, year, winner, second_place, best_player) VALUES (%s, %s, %s, %s, %s)", (name, year, winner, second_place, best_player))
+    cursor.execute("INSERT INTO tournaments (name, year, winner, best_player) VALUES (%s, %s, %s, %s)", (name, year, winner, best_player))
     cursor.connection.commit()
 
     close_connection(cursor)
@@ -61,8 +61,8 @@ def delete_tournament(id):
 
     close_connection(cursor)
 
-def find_tournament(nameFind, yearFind, winnerFind, second_placeFind, best_playerFind):
-    statement = """SELECT * FROM TOURNAMENTS WHERE(NAME LIKE  '{}%' ) AND (YEAR LIKE '{}%' ) AND (WINNER LIKE '{}%' ) AND (SECOND_PLACE LIKE '{}%' ) AND (CAST(BEST_PLAYER AS VARCHAR(9)) LIKE '{}%' )""".format(nameFind, yearFind, winnerFind, second_placeFind, best_playerFind)
+def find_tournament(nameFind, yearFind, winnerFind, best_playerFind):
+    statement = """SELECT * FROM TOURNAMENTS WHERE(NAME LIKE  '{}%' ) AND (YEAR LIKE '{}%' ) AND (CAST(WINNER AS VARCHAR(20)) LIKE '{}%' )  AND (CAST(BEST_PLAYER AS VARCHAR(9)) LIKE '{}%' )""".format(nameFind, yearFind, winnerFind, best_playerFind)
 
     cursor = create_connection()
     cursor.execute(statement)
@@ -73,9 +73,9 @@ def find_tournament(nameFind, yearFind, winnerFind, second_placeFind, best_playe
 
     return tournaments
 
-def update_tournament(id, nameUpdate, yearUpdate, winnerUpdate, second_placeUpdate, best_playerUpdate):
+def update_tournament(id, nameUpdate, yearUpdate, winnerUpdate, best_playerUpdate):
     cursor = create_connection()
-    statement = """UPDATE TOURNAMENTS SET NAME = '{}', YEAR = '{}', WINNER = '{}', SECOND_PLACE = '{}', BEST_PLAYER = {} WHERE ID={} """.format(nameUpdate, yearUpdate, winnerUpdate, second_placeUpdate, best_playerUpdate, id)
+    statement = """UPDATE TOURNAMENTS SET NAME = '{}', YEAR = '{}', WINNER = '{}', BEST_PLAYER = {} WHERE ID={} """.format(nameUpdate, yearUpdate, winnerUpdate, best_playerUpdate, id)
     cursor.execute(statement)
     cursor.connection.commit()
 
@@ -83,7 +83,7 @@ def update_tournament(id, nameUpdate, yearUpdate, winnerUpdate, second_placeUpda
 
 def showJointTables():
     cursor = create_connection()
-    statement= """ SELECT TOURNAMENTS.ID, TOURNAMENTS.NAME, YEAR, WINNER, SECOND_PLACE, PLAYERS.NAME FROM TOURNAMENTS INNER JOIN PLAYERS ON PLAYERS.ID=TOURNAMENTS.BEST_PLAYER  """
+    statement= """ SELECT TOURNAMENTS.ID, TOURNAMENTS.NAME, YEAR, TEAMS.NATION , PLAYERS.NAME FROM TOURNAMENTS INNER JOIN PLAYERS ON PLAYERS.ID=TOURNAMENTS.BEST_PLAYER INNER JOIN TEAMS ON TEAMS.ID=TOURNAMENTS.WINNER  """
     cursor.execute(statement)
     tournaments = cursor.fetchall()
     cursor.connection.commit()
@@ -91,8 +91,8 @@ def showJointTables():
     close_connection(cursor)
     return tournaments
 
-def findInJointTables(nameFind, yearFind, winnerFind, second_placeFind, best_playerFind):
-    statement= """ SELECT TOURNAMENTS.ID, TOURNAMENTS.NAME, YEAR, WINNER, SECOND_PLACE, PLAYERS.NAME FROM TOURNAMENTS INNER JOIN PLAYERS ON PLAYERS.ID=TOURNAMENTS.BEST_PLAYER WHERE(TOURNAMENTS.NAME LIKE  '{}%' ) AND (YEAR LIKE '{}%' ) AND (WINNER LIKE '{}%' ) AND (SECOND_PLACE LIKE '{}%' ) AND (PLAYERS.NAME LIKE '{}%' )""".format(nameFind, yearFind, winnerFind, second_placeFind, best_playerFind)
+def findInJointTables(nameFind, yearFind, winnerFind, best_playerFind):
+    statement= """ SELECT TOURNAMENTS.ID, TOURNAMENTS.NAME, YEAR, TEAMS.NATION , PLAYERS.NAME FROM TOURNAMENTS INNER JOIN PLAYERS ON PLAYERS.ID=TOURNAMENTS.BEST_PLAYER INNER JOIN TEAMS ON TEAMS.ID=TOURNAMENTS.WINNER WHERE(TOURNAMENTS.NAME LIKE  '{}%' ) AND (YEAR LIKE '{}%' ) AND (TEAMS.NATION LIKE '{}%' )  AND (PLAYERS.NAME LIKE '{}%' )""".format(nameFind, yearFind, winnerFind, best_playerFind)
 
     cursor = create_connection()
     cursor.execute(statement)
@@ -108,6 +108,7 @@ def tournaments():
 
     #create_table()
     #allPlayers = players.get_players()
+    allTeams=teams.get_teams()
     dsn = app.config['dsn']
 
     app.store = StoreP(dsn)
@@ -117,22 +118,21 @@ def tournaments():
         #all_tournaments = get_tournaments() # get all tournaments
         all_tournaments = showJointTables()
         #queriedTournaments = find_tournament('?','?','?','?','?')
-        queriedTournaments = findInJointTables('?','?','?','?','?')
+        queriedTournaments = findInJointTables('?','?','?','?')
     elif 'add' in request.form:
         # ----------------------------------------------
         name = request.form['name']
         year = request.form['year']
         winner = request.form['winner']
-        second_place = request.form['second_place']
         best_player = request.form['best_player']
         # ----------------------------------------------
 
-        add_new_tournament(name, year, winner, second_place, best_player) # save to db
+        add_new_tournament(name, year, winner, best_player) # save to db
 
         #all_tournaments = get_tournaments() # get all tournaments
         all_tournaments = showJointTables()
         #queriedTournaments = find_tournament('?','?','?','?','?')
-        queriedTournaments = findInJointTables('?','?','?','?','?')
+        queriedTournaments = findInJointTables('?','?','?','?')
     elif 'delete' in request.form:
         ids = request.form.getlist('tournaments_to_delete')
         for id in ids:
@@ -140,30 +140,28 @@ def tournaments():
         #all_tournaments = get_tournaments()
         all_tournaments = showJointTables()
         #queriedTournaments = find_tournament('?','?','?','?','?')
-        queriedTournaments = findInJointTables('?','?','?','?','?')
+        queriedTournaments = findInJointTables('?','?','?','?')
     elif 'find' in request.form:
         nameFind = request.form['nameFind']
         yearFind = request.form['yearFind']
         winnerFind = request.form['winnerFind']
-        second_placeFind = request.form['second_placeFind']
         best_playerFind = request.form['best_playerFind']
 
         #all_tournaments = get_tournaments()
         all_tournaments = showJointTables()
         #queriedTournaments = find_tournament(nameFind,yearFind,winnerFind,second_placeFind,best_playerFind)
-        queriedTournaments = findInJointTables(nameFind,yearFind,winnerFind,second_placeFind,best_playerFind)
+        queriedTournaments = findInJointTables(nameFind,yearFind,winnerFind,best_playerFind)
     elif 'update' in request.form:
         ids = request.form.getlist('update')
         for id in ids:
             nameUpdate = request.form['nameUpdate'+id]
             yearUpdate = request.form['yearUpdate'+id]
             winnerUpdate = request.form['winnerUpdate'+id]
-            second_placeUpdate = request.form['second_placeUpdate'+id]
             best_playerUpdate = request.form['best_playerUpdate'+id]
-            update_tournament(id, nameUpdate, yearUpdate, winnerUpdate, second_placeUpdate, best_playerUpdate)
+            update_tournament(id, nameUpdate, yearUpdate, winnerUpdate, best_playerUpdate)
 
         #all_tournaments = get_tournaments()
         all_tournaments = showJointTables()
-        queriedTournaments = findInJointTables('?','?','?','?','?')
+        queriedTournaments = findInJointTables('?','?','?','?')
         #queriedTournaments = find_tournament('?','?','?','?','?')
-    return render_template("tournaments.html", tournaments=all_tournaments, tournamentsToShow=queriedTournaments, PlayersSelect=allPlayers)
+    return render_template("tournaments.html", tournaments=all_tournaments, tournamentsToShow=queriedTournaments, PlayersSelect=allPlayers, TeamsSelect=allTeams)
